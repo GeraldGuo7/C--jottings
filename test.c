722fd820fd2d910b0a622c1e2b79914097e29bee
GeraldGuo7 <83147186+GeraldGuo7@users.noreply.github.com>
2022-06-09 12:39:12 +0800

    Add log files

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "log.h"
+
+int main(int argv, char **argc)
+{
+    printf("%s\n", argc[0]);
+    LogWrite(INFO, "%s", "Hello World!");
+    LogWrite(DEBUG, "%s", "H.e.l.l.o W.o.r.l.d!");
+    LogWrite(WARN, "%s", "H e l l o W o r l d!");
+    LogWrite(ERROR, "%s", "Hello World!");
+    return 0;
+}
+
